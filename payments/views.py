622b0254a64e33bfce8a1d996eb530c2ac13d8cf
Zhang Dong <zhangdong.1995@outlook.com>
2020-07-31 18:23:53 -0400

    API - Customer operations for update, delele, retrieve

--- payments/views.py
+++ payments/views.py
@@ -27,7 +27,47 @@ def add_customer(request):
 
 @csrf_exempt
 def customers(request):
-    pass
+    data = json.loads(request.body)
+    customer_id = data.get('customer_id')
+
+    if request.method == 'GET':
+
+        try:
+            response = stripe.Customer.retrieve(customer_id)
+        except:
+            message = "Customer not exists. Please try again."
+            return JsonResponse({'message': message}, status=status.HTTP_404_NOT_FOUND)
+
+        dict = {}
+
+        dict["name"] = response["name"]
+        dict["email"] = response["email"]
+        dict["customer_id"] = response['id']
+        dict["default_source"] = response["default_source"]
+        if response.get("source"):
+            dict["address"] = response["sources"]["data"][0]["owner"]["address"]
+
+        return JsonResponse(dict, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        del data['customer_id']
+
+        try:
+            stripe.Customer.modify(customer_id, description=data.get('description'))
+        except:
+            message = "Customer not exists. Please try again."
+            return JsonResponse({'message': message}, status=status.HTTP_404_NOT_FOUND)
+
+        return JsonResponse({'message': "Update successfully!"}, status=status.HTTP_200_OK)
+
+    if request.method == 'DELETE':
+        try:
+            stripe.Customer.delete(customer_id)
+        except:
+            message = "Customer not exists. Please try again."
+            return JsonResponse({'message': message}, status=status.HTTP_404_NOT_FOUND)
+
+        return JsonResponse({'message': "Delete successfully!"}, status=status.HTTP_200_OK)
 
 
 @csrf_exempt
